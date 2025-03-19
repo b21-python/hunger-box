cc2fc861702b05ddd79f92aa1fc4d4f48df18499
Colin Kerr <cgkerr27@users.noreply.github.com>
2025-03-19 13:25:33 -0400

    made hurd chase or run away

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pygame
 from math import pi
+import random
 
 pygame.init() # initialize pygame
 pygame.font.init()
@@ -17,6 +18,7 @@ player_pos = pygame.Vector2(screen.get_width()/2, screen.get_height()/2)
 player_health = 80
 player_size = player_health // 2
 
+hurd_limit = 20
 hurd = [pygame.Rect(0,0,5,5), pygame.Rect(600,300,5,5), pygame.Rect(100, 100, 5, 5)]
 
 portal = pygame.Rect(400, 250, 60, 60)
@@ -30,35 +32,48 @@ while running:
     # Set screen color
     screen.fill([255,255,255])
 
+    pygame.draw.rect(screen, [100,100,100], portal)
+
     # Draw the player
     player_rect = pygame.draw.rect(screen, [0,0,0], (player_pos.x, player_pos.y, player_size, player_size))
 
-    for h in hurd:
-        pygame.draw.rect(screen, [0,0,0], h)
+    #if len(hurd) <= hurd_limit:
         
-    
+
     index = player_rect.collidelist(hurd)
-    if index > 0:
+    if index >= 0:
         beast = hurd[index]
         player_size += beast.width//2
         hurd.pop(index)
-        
-    pygame.draw.rect(screen, [0,0,0], portal)
+
+    for h in hurd:
+        pygame.draw.rect(screen, [0,0,0], h)
+        direction = -1
+        if player_rect.width >= portal.width:
+            direction = 1
+        x = direction
+        y = direction
+        if player_pos.x < h.x:
+            x = -x
+        if player_pos.y < h.y:
+            y = -y
+        h.move_ip(x,y)
+            
     
-    if portal.contains(player_rect):
+    if portal.contains(player_rect) and portal.width - player_rect.width < 5 :
         running = False
         win = True
 
     # update player position based on keys pressed
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player_pos.y -= 10
+        player_pos.y -= 1
     if keys[pygame.K_s]:
-        player_pos.y += 10
+        player_pos.y += 1
     if keys[pygame.K_a]:
-        player_pos.x -= 10
+        player_pos.x -= 1
     if keys[pygame.K_d]:
-        player_pos.x += 10
+        player_pos.x += 1
         
     player_health -= 0.1
     player_size = player_health // 2
